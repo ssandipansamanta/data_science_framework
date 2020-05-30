de24f8f24d8092d7663a72fd35173d529791288c
Sandipan Samanta <42437305+ssandipansamanta@users.noreply.github.com>
2020-05-30 16:13:44 +0200

    All tables and their Fields extracted for a schema

---
+++ src/scala/all_table_information.scala
@@ -0,0 +1,23 @@
+%scala 
+import spark.implicits._
+import org.apache.spark.sql.functions.{col, count, lit, rank, _}
+import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType}
+import org.apache.spark.sql.Row
+
+val schema = StructType(
+    StructField("value", StringType, true) ::
+    StructField("table_name", StringType, false) ::
+    StructField("schema_name", StringType, false) :: Nil)
+
+var allTable = spark.createDataFrame(sc.emptyRDD[Row], schema) //spark.createDataFrame(spark.sparkContext.emptyRDD[Row], schema)
+
+val schemaName = "SCHEMA_NAME"
+val names = Seq("TABLE_NAME_1",	"TABLE_NAME_2")
+// val _c = 0
+for (tableNames <- names) {
+  val selectTables = spark.read.table(schemaName.concat(".".concat(tableNames))).limit(1)  
+  val selectColumns = selectTables.columns.toList
+  val columns_df = selectColumns.toDF
+  val columnsWithTableName = columns_df.select(col("value"),lit(tableNames).as("table_name"), lit(schemaName).as("schema_name"))
+  allTable = allTable.union(columnsWithTableName)
+}
